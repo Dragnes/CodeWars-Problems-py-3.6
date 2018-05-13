208575e8fdc81e7e721ba766a619baaf38e47b72
Dhruv Sengar <sengardhruv101@gmail.com>
2018-05-13 18:33:51 -0400

    Update Codewars Problem set 4.py

--- Codewars Problem set 4.py
+++ Codewars Problem set 4.py
@@ -360,7 +360,7 @@ def maxSequence(arr):
 print(maxSequence(array))
 
 
-# 65) Write a function called that takes a string of parentheses and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid
+# 65) Write a function that takes a string of parentheses and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid
 #     ex: "()"              =>  true
 #     ex: ")(()))"          =>  false
 #     ex: "("               =>  false
@@ -375,8 +375,8 @@ def valid_parentheses(string):
         if i == ")":
             count -= 1
         if count < 0:
-            return False
-    return count == 0
+            return(False)
+    return(count == 0)
 print(valid_parentheses(string))
 
 string = "(())((()()()))"
